a1b08146585adf3b95a936fa322c885026638d9c
Kevin Herembourg <kevin.herembourg@gmail.com>
2021-05-21 17:15:36 +0200

    Version 2.4.7 of SDK

--- sample-kotlin/src/main/java/io/purchasely/sample/kotlin/SubscriptionsActivity.kt
+++ sample-kotlin/src/main/java/io/purchasely/sample/kotlin/SubscriptionsActivity.kt
@@ -1,19 +1,10 @@
 package io.purchasely.sample.kotlin
 
 import android.os.Bundle
-import android.util.Log
-import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.Observer
-import com.google.android.material.snackbar.Snackbar
-import io.purchasely.ext.PLYAlertMessage
-import io.purchasely.ext.PLYUIFragmentType
 import io.purchasely.ext.Purchasely
-import io.purchasely.ext.UIListener
 import io.purchasely.sample.R
-import io.purchasely.views.subscriptions.PLYSubscriptionsFragment
 import kotlinx.android.synthetic.main.activity_feature_list.*
 
 class SubscriptionsActivity : FragmentActivity() {
@@ -23,8 +14,12 @@ class SubscriptionsActivity : FragmentActivity() {
         setContentView(R.layout.activity_feature_list)
 
         supportFragmentManager.beginTransaction()
-                .addToBackStack(null)
-                .replace(R.id.inappFragment, PLYSubscriptionsFragment(), "SubscriptionsFragment")
+            .addToBackStack(null)
+            .replace(
+                R.id.inappFragment,
+                Purchasely.subscriptionsFragment(),
+                "SubscriptionsFragment"
+            )
                 .commitAllowingStateLoss()
 
         progressBar.isVisible = false
